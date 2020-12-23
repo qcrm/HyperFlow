21c2a824325dcb4a28f2dc42c004d62b7184588a
Michael Halls-Moore <mike@quarkgluon.com>
2020-12-23 21:42:53 +0000

    Added Scheme abstract base class.

---
+++ src/fvm_2d_cart/scheme/scheme/scheme.cpp
@@ -0,0 +1,18 @@
+#ifndef __SCHEME_CPP
+#define __SCHEME_CPP
+
+#include "scheme.h"
+
+namespace HyperFlow {
+
+/* Constructor */
+Scheme::Scheme()
+{}
+
+/* Destructor */
+Scheme::~Scheme()
+{}
+
+}
+
+#endif
