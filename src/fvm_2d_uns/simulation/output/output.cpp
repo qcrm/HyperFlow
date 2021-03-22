4522cc856bfb41a5125441bb726b8384cff84598
Michael Halls-Moore <mike@quarkgluon.com>
2021-03-22 12:44:49 +0000

    Added Euler Ideal Gas output class.

---
+++ src/fvm_2d_uns/simulation/output/output.cpp
@@ -0,0 +1,85 @@
+#ifndef __OUTPUT_CPP
+#define __OUTPUT_CPP
+
+#include "output.h"
+
+namespace HyperFlow {
+
+/* Constructor */
+EulerIdealGasDataOutput::EulerIdealGasDataOutput()
+{}
+
+/* Constructor supplying spatio-temporal extents
+ * for results metadata */
+EulerIdealGasDataOutput::EulerIdealGasDataOutput(const std::shared_ptr<EulerIdealGasModel> _model,
+                                                 const double _x_left,
+                                                 const double _x_right,
+                                                 const double _y_bottom,
+                                                 const double _y_top,
+                                                 const double _t_start,
+                                                 const double _t_end)
+:
+    model(_model),
+    x_left(_x_left),
+    x_right(_x_right),
+    y_bottom(_y_bottom),
+    y_top(_y_top),
+    t_start(_t_start),
+    t_end(_t_end)
+{}
+
+/* Destructor */
+EulerIdealGasDataOutput::~EulerIdealGasDataOutput() {};
+
+/* Output data to file */
+void EulerIdealGasDataOutput::to_file(const std::shared_ptr<Mesh>& mesh,
+                                      const unsigned int n,
+                                      const double t)
+{
+    char filename[19];
+    sprintf(filename, "results_%06d.csv", n);
+    std::string header = "x,y,rho,rho_u,rho_v,E,u,v,p,e,M";
+ 
+    std::ofstream out(filename);
+
+    // Add metadata
+    out << "X_LEFT=" << x_left << std::endl;
+    out << "X_RIGHT=" << x_right << std::endl;
+    out << "Y_BOTTOM=" << y_bottom << std::endl;
+    out << "Y_TOP=" << y_top << std::endl;
+    out << "T_START=" << t_start << std::endl;
+    out << "T_END=" << t_end << std::endl;
+    out << "T=" << t << std::endl << std::endl;
+
+    out << header << std::endl; 
+
+    CellVec1D cells = mesh->get_cells();
+
+    for (unsigned int cell_idx=0; cell_idx<cells.size(); cell_idx++) {
+        Cell cell = cells[cell_idx];
+        Vec1D centroid = cell.get_centroid();
+        Vec1D cons = cell.get_flow_values();
+        Vec1D prim = model->cons_to_prim(cons);
+
+        double mach_number = model->prim_mach_number(prim);
+
+        out << centroid[0] << ",";                          // x-coordinate
+        out << centroid[1] << ",";                          // y-coordinate
+        out << cons[0] << ",";                              // Density (rho)
+        out << cons[1] << ",";                              // x-Momentum (rho_u)
+        out << cons[2] << ",";                              // y-Momentum (rho_v)
+        out << cons[3] << ",";                              // Total energy (E)
+        out << prim[1] << ",";                              // x-Velocity (u)
+        out << prim[2] << ",";                              // v-Velocity (v)
+        out << prim[3] << ",";                              // Pressure (p)
+        out << model->prim_internal_energy(prim) << ",";    // Internal energy (e)
+        out << mach_number;                                 // Mach number
+        out << std::endl;
+    }
+
+    out.close();
+}
+
+}
+
+#endif
---
+++ src/fvm_2d_uns/simulation/output/output.h
@@ -0,0 +1,63 @@
+#ifndef __OUTPUT_H
+#define __OUTPUT_H
+
+#include <fstream>
+
+#include "../../model/euler_ideal_gas/euler_ideal_gas.h"
+#include "../../mesh/mesh/mesh.h"
+
+namespace HyperFlow {
+
+class EulerIdealGasDataOutput
+{
+    
+    public:
+        
+        /* Constructor */
+        EulerIdealGasDataOutput();
+        
+        /* Constructor supplying spatio-temporal extents
+         * for results metadata */
+        EulerIdealGasDataOutput(const std::shared_ptr<EulerIdealGasModel> _model,
+                                const double _x_left,
+                                const double _x_right,
+                                const double _y_bottom,
+                                const double _y_top,
+                                const double _t_start,
+                                const double _t_end);
+        
+        /* Destructor */
+        virtual ~EulerIdealGasDataOutput();
+        
+        /* Output data to file */
+        void to_file(const std::shared_ptr<Mesh>& mesh,
+                     const unsigned int n,
+                     const double t);
+
+    private:
+       
+        /* Pointer to the model equations */
+        std::shared_ptr<EulerIdealGasModel> model;
+        
+        /* Left domain extent */
+        double x_left;
+        
+        /* Right domain extent */
+        double x_right;
+        
+        /* Bottom domain extent */
+        double y_bottom;
+        
+        /* Top domain extent */
+        double y_top;
+        
+        /* Start time of the simulation */
+        double t_start;
+        
+        /* End time of the simulation */
+        double t_end;
+};
+
+}
+
+#endif
