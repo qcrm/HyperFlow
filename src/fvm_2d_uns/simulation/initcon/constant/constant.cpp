2180db5f71c106e2edfeccc03896775381c1d83c
Michael Halls-Moore <mike@quarkgluon.com>
2021-03-22 11:56:04 +0000

    Added initial condition class, along with constant initial condition derived subclass.

---
+++ src/fvm_2d_uns/simulation/initcon/constant/constant.cpp
@@ -0,0 +1,48 @@
+#ifndef __CONSTANT_INITCON_CPP
+#define __CONSTANT_INITCON_CPP
+
+#include "constant.h"
+
+namespace HyperFlow {
+
+/* Constructor */
+ConstantInitialCondition::ConstantInitialCondition()
+{}
+
+/* Constructor with constant initial conservative flow state */
+ConstantInitialCondition::ConstantInitialCondition(
+    const double _x_left,
+    const double _x_right,
+    const double _y_bottom,
+    const double _y_top,
+    const double _t_start,
+    const double _t_end,
+    const Vec1D& _cons_init_state)
+:
+    InitialCondition(_x_left, 
+                     _x_right,
+                     _y_bottom,
+                     _y_top,
+                     _t_start,
+                     _t_end,
+                     _cons_init_state.size()),
+    cons_init_state(_cons_init_state)
+{}
+ 
+/* Destructor */
+ConstantInitialCondition::~ConstantInitialCondition()
+{}
+
+/* Return the function for a particular dimension that
+         * determines the constant flow values */
+std::function<double (const double, const double)> ConstantInitialCondition::field_func_dim(
+    const unsigned int dim
+) {
+    return [this, dim](const double x, const double y) { 
+        return cons_init_state[dim];
+    };
+}
+
+}
+
+#endif
---
+++ src/fvm_2d_uns/simulation/initcon/initcon/initcon.h
@@ -0,0 +1,85 @@
+#ifndef __INITCON_H
+#define __INITCON_H
+
+#include <functional>
+#include <memory>
+
+#include "../../../../share/tensor/tensor.h"
+
+namespace HyperFlow {
+
+class InitialCondition
+{
+
+    public:
+       
+        /* Constructor */
+        InitialCondition();
+        
+        /* Constructor from spatial and temporal extents */
+        InitialCondition(const double _x_left,
+                         const double _x_right,
+                         const double _y_bottom,
+                         const double _y_top,
+                         const double _t_start,
+                         const double _t_end,
+                         const unsigned int _dimension);
+            
+        /* Destructor */
+        virtual ~InitialCondition();
+
+        /* Obtain the initial flow values at the
+         * provided coordinates */
+        virtual Vec1D operator() (const double x, const double y);
+        
+        /* Obtain the left extent of the simulation */
+        virtual double get_x_left();
+        
+        /* Obtain the right extent of the simultion */
+        virtual double get_x_right();
+        
+        /* Obtain the bottom extent of the simulation */
+        virtual double get_y_bottom();
+        
+        /* Obtain the top extent of the simulation */
+        virtual double get_y_top();
+        
+        /* Obtain the simualation start time */
+        virtual double get_t_start();
+        
+        /* Obtain the simulation end time */
+        virtual double get_t_end();
+        
+        /* Obtain the dimensionality of the model system */
+        virtual unsigned int get_dimension();
+
+    protected:
+        
+        /* Left extent of the simulation */
+        double x_left;
+        
+        /* Right extent of the simulation */
+        double x_right;
+        
+        /* Bottom extent of the simulation */
+        double y_bottom;
+        
+        /* Top extent of the simulation */
+        double y_top;
+        
+        /* Start time of the simulation */
+        double t_start;
+        
+        /* End time of the simulation */
+        double t_end;
+        
+        /* Dimensionality of the model system */
+        unsigned int dimension;
+
+        /* Return the flow value function at a particular dimension */
+        virtual std::function<double (const double, const double)> field_func_dim(const unsigned int dim) = 0;
+};
+
+}
+
+# endif
